e3b0831c5155565cc168fa3ea906e56f58868f7d
Salschoka <mimutsuyo3624@gmail.com>
2020-11-19 11:39:18 +0900

    Add Simplest UI and trying to use mysql2

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mysql2 = require('mysql2');
+
+const sql_connection = mysql2.createConnection({
+	//This is just my case, You must reconfigure when use This repo!
+	host      :'192.168.10.20',
+	user      :'node',
+	password  :'secret'
+})
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -22,20 +31,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+sql_connection.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+
+	console.log('DB connected as id ' + sql_connection.threadId);
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
